13f77fc4fb07d0eb5ddce0c5b55f18977137c091
DimalshaKavindi <dimalshakavindi2020@gmail.com>
2024-07-04 15:18:23 +0530

    Revert "commit"
    This reverts commit 271cef35a5751a4a914e0f444f7a9137ba46803a.

--- EVORA MOMENTS/frontend/src/Components/Home.js
+++ EVORA MOMENTS/frontend/src/Components/Home.js
@@ -1,49 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { BsFillChatSquareHeartFill } from "react-icons/bs";
-import { FaGlassCheers } from "react-icons/fa";
-import { FaImages } from "react-icons/fa6";
-import { GiDiamondRing } from "react-icons/gi";
-import { animated, useSpring } from "react-spring";
+import 'animate.css/animate.min.css';
+import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function Home() {
-  const [showBoxes, setShowBoxes] = useState(false);
-
   const headerStyle = {
     background: `url('./Images/back1.png')`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    position: "relative",
-    height: "600px",
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    position: 'relative',
+    height: '600px',
   };
 
   const overlayStyle = {
-    position: "absolute",
-    top: "0",
-    left: "0",
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
   };
 
-  const handleScroll = () => {
-    if (!showBoxes && window.scrollY > 400) {
-      setShowBoxes(true);
-    }
+  const contentContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '10%',
+    transform: 'translateY(-50%)',
   };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []); 
 
   const contentStyle = {
     backgroundColor: "#EDE0D4",
-    padding: "20px",
+    padding: '20px',
   };
 
   const gridContainerStyle = {
@@ -58,23 +49,6 @@ function Home() {
     padding: "20px",
   };
 
-  const contentContainerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    top: '50%',
-    left: '10%',
-    transform: 'translateY(-50%)',
-  };
-
-  const revealProps = useSpring({
-    opacity: showBoxes ? 1 : 0,
-    transform: showBoxes ? "translateY(0)" : "translateY(50px)",
-    from: { opacity: 0, transform: "translateY(50px)" },
-  });
-
   return (
     <div>
       <header style={headerStyle}>
@@ -86,57 +60,44 @@ function Home() {
           </form>
         </div>
       </header>
-      <div style={contentStyle}>
+      <div style={contentStyle} >
         <div style={{ marginBottom: "60px" }}>
           <p></p>
-          <p
-            className="font-weight-bold"
-            style={{
-              fontFamily: "Khand, sans-serif",
-              color: "#DEB887",
-              fontSize: "20px",
-              justifyContent: "center",
-              alignItems: "center",
-              position: "absolute",
-              left: "8%",
-              marginBottom: "100px",
-            }}
-          >
+          <p className="font-weight-bold" style={{ fontFamily: 'Khand, sans-serif', color: '#DEB887', fontSize: "20px", justifyContent: 'center', alignItems: 'center', position: 'absolute', left: '8%', marginBottom: '100px', }}>
             OVERVIEW
           </p>
         </div>
 
-        <div style={{ marginLeft: "12%", marginRight: "12%" }}>
+        <div style={{ marginLeft: '12%', marginRight: "12%" }}>
           <div style={gridContainerStyle}>
-            <animated.div style={{ ...boxStyle, ...revealProps }}>
 
-              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px"}}><FaGlassCheers style={{fontSize:"40px", color:'#9c6644'}}/>WEDDING SHOOTS</h1>
+            <div style={boxStyle}>
+            <img src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.webp" class="animated bounce infinite" alt="Transparent MDB Logo" id="animated-img1"/>
+              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px" }}>WEDDING SHOOTS</h1>
               <p>Many of us have a story to tell and with our wedding photography, we can tell your story for years to come. We’ll help you preserve the details, moments, and emotions.</p>
-            </animated.div>
+            </div>
 
-            
-            <animated.div style={{ ...boxStyle, ...revealProps }}>
-              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px" }}><GiDiamondRing style={{fontSize:"40px", color:'#9c6644'}}/>ENGAGEMENTS</h1>
+
+            <div style={boxStyle}>
+              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px" }}>ENGAGEMENTS</h1>
               <p>Engagements are a unique way to capture the love between two people. Capturing the emotions of the moment and freezing them in time is what engagement photography is all about.</p>
-            </animated.div>
+            </div>
 
 
-            
-            <animated.div style={{ ...boxStyle, ...revealProps }}>
-              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px" }}><BsFillChatSquareHeartFill style={{fontSize:"30px", color:'#9c6644'}}/>CASUAL SHOOTS</h1>
+            <div style={boxStyle}>
+              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px" }}>CASUAL SHOOTS</h1>
               <p>Casual shoots are a great way to get comfortable in front of the camera and to get some practice in. We do offer casual shoots and they are a great way to get started.</p>
-            </animated.div>
+            </div>
 
-            
-            <animated.div style={{ ...boxStyle, ...revealProps }}>
-              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px" }}><FaImages style={{fontSize:"40px", color:'#9c6644'}}/>EVENTS</h1>
+            <div style={boxStyle}>
+              <h1 style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px" }}>EVENTS</h1>
               <p>Whether it s a corporate gathering or a milestone celebration, our event photography preserves the atmosphere and energy of the occasion.</p>
-            </animated.div>
+            </div>
           </div>
         </div>
       </div>
 
-      <Footer />
+      <Footer/>
     </div>
   );
 }
